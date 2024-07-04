563f902585990c0ebc5b78a5c22981fd2d4146d9
Nogunfnn <40021976+Nogunfnn@users.noreply.github.com>
2024-07-04 17:06:18 +0900

    Update lastmod.ts

--- quartz/plugins/transformers/lastmod.ts
+++ quartz/plugins/transformers/lastmod.ts
@@ -5,7 +5,7 @@ import { QuartzTransformerPlugin } from "../types"
 import chalk from "chalk"
 
 export interface Options {
-  priority: ("frontmatter" | "git" | "filesystem")[]
+  priority: ()[]
 }
 
 const defaultOptions: Options = {

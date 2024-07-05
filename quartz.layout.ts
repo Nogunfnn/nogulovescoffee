645c635b321d681692d266ce651d381561fd6e36
Nogunfnn <40021976+Nogunfnn@users.noreply.github.com>
2024-07-05 11:41:06 +0900

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -20,7 +20,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
-    Component.Folderlist(),
+    Component.taglist(),
   ],
   left: [
     Component.PageTitle(),
